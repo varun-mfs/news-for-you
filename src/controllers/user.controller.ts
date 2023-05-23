538930a9090824e77341eaacc6c094c364a677da
varun <varun.sharma@mindfiresolutions.com>
2023-05-23 13:37:47 +0530

    add user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from '../services/user.service';
-// import { User } from '@prisma/client';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // @Get('/all')
-  // async getUsers(): Promise<User[]> {
-  //   return await this.userService.getUsers();
-  // }
+  @Get('/all')
+  async getUsers(): Promise<User[]> {
+    return await this.userService.getUsers();
+  }
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from '../dao/user.repository';
-// import { User } from '@prisma/client';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
   // getUsers(): string {
-  // async getUsers(): Promise<User[]> {
-  //   // return 'Hello World!!';
-  //   // return this.userService.getUsers();
-  //   return await this.userRepository.getAllUsers();
-  // }
+  async getUsers(): Promise<User[]> {
+    // return 'Hello World!!';
+    // return this.userService.getUsers();
+    console.log('Inside user service');
+    return await this.userRepository.getAllUsers();
+  }
 }
